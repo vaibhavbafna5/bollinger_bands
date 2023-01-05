f94bff3c0814011644e9bfaadb2881d410e67058
vaibhavbafna5 <vbafna@umich.edu>
2023-01-05 16:29:44 -0500

    commit

--- vti_super_trend.py
+++ vti_super_trend.py
@@ -3,5 +3,5 @@ from super_trend_lib import SuperTrendRunner
 # JSON_PATH = '/Users/vaibhav/projects/super_trend'
 JSON_PATH = '/root/projects/supertrend'
 
-vti_runner = SuperTrendRunner(ticker='VTI', json_path=JSON_PATH, debug_mode=True)
+vti_runner = SuperTrendRunner(ticker='VTI', json_path=JSON_PATH, debug_mode=False)
 vti_runner.execute_daily_trade_decision()
