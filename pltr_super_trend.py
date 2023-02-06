8a3d424df656f8542709b365d5f7214a0878e142
vaibhavbafna5 <vbafna@umich.edu>
2023-02-06 17:44:37 -0500

    commit

--- pltr_super_trend.py
+++
@@ -1,7 +0,0 @@
-from super_trend_lib import SuperTrendRunner
-
-# JSON_PATH = '/Users/vaibhav/projects/super_trend'
-JSON_PATH = '/root/projects/supertrend'
-
-pltr_runner = SuperTrendRunner(ticker='PLTR', json_path=JSON_PATH, debug_mode=False)
-pltr_runner.execute_daily_trade_decision()
--- vgt_super_trend.py
+++
@@ -1,7 +0,0 @@
-from super_trend_lib import SuperTrendRunner
-
-# JSON_PATH = '/Users/vaibhav/projects/super_trend'
-JSON_PATH = '/root/projects/supertrend'
-
-vgt_runner = SuperTrendRunner(ticker='VGT', json_path=JSON_PATH, debug_mode=False)
-vgt_runner.execute_daily_trade_decision()
